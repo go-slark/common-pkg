b0155a12793aa6171d9a196dc7ce6966b4f862b2
blackroot <longkuicheng@class100.com>
2020-12-24 10:05:30 +0800

    validate mobile

--- xvalidator/custom.go
+++ xvalidator/custom.go
@@ -4,11 +4,13 @@ import (
 	"github.com/gin-gonic/gin/binding"
 	"github.com/globalsign/mgo/bson"
 	"gopkg.in/go-playground/validator.v9"
+	"regexp"
 )
 
 func RegisterCustomValidator() {
 	v := binding.Validator.Engine().(*validator.Validate)
 	v.RegisterValidation("is-objectid", ValidateObjectId)
+	v.RegisterValidation("ValidateMobile", ValidateMobile)
 }
 
 func ValidateObjectId(fl validator.FieldLevel) bool {
@@ -16,6 +18,20 @@ func ValidateObjectId(fl validator.FieldLevel) bool {
 		if idStr == "" || bson.IsObjectIdHex(idStr) {
 			return true
 		}
+	}
+	return false
+}
+
+func ValidateMobile(fl validator.FieldLevel) bool {
+	regularExp := "/(^(0[0-9]{2,3}\\-)?([2-9][0-9]{6,7})+(\\-[0-9]{1,4})?$)|(^((\\(\\d{3}\\))|(\\d{3}\\-))?(1[3578]\\d{9})$)|(^(400)-(\\d{3})-(\\d{4})(.)(\\d{1,4})$)|(^(400)-(\\d{3})-(\\d{4}$))/"
+	regExp := regexp.MustCompile(regularExp)
+	mobile, ok := fl.Field().Interface().(string)
+	if !ok {
+		return false
+	}
+
+	if regExp.MatchString(mobile) {
+		return true
 	}
 	return false
 }
