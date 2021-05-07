ef155348e0c8f85a675fec42b1a6c83330de4570
class100 <longkuicheng@class100.com>
2021-05-07 17:35:59 +0800

    validate id card

--- xvalidator/custom.go
+++ xvalidator/custom.go
@@ -11,6 +11,7 @@ func RegisterCustomValidator() {
 	v := binding.Validator.Engine().(*validator.Validate)
 	v.RegisterValidation("is-objectid", ValidateObjectId)
 	v.RegisterValidation("ValidateMobile", ValidateMobile)
+	v.RegisterValidation("ValidateIdCard", ValidateIdCard)
 }
 
 func ValidateObjectId(fl validator.FieldLevel) bool {
@@ -24,6 +25,20 @@ func ValidateObjectId(fl validator.FieldLevel) bool {
 
 func ValidateMobile(fl validator.FieldLevel) bool {
 	regularExp := "/(^(0[0-9]{2,3}\\-)?([2-9][0-9]{6,7})+(\\-[0-9]{1,4})?$)|(^((\\(\\d{3}\\))|(\\d{3}\\-))?(1[3578]\\d{9})$)|(^(400)-(\\d{3})-(\\d{4})(.)(\\d{1,4})$)|(^(400)-(\\d{3})-(\\d{4}$))/"
+	regExp := regexp.MustCompile(regularExp)
+	mobile, ok := fl.Field().Interface().(string)
+	if !ok {
+		return false
+	}
+
+	if regExp.MatchString(mobile) {
+		return true
+	}
+	return false
+}
+
+func ValidateIdCard(fl validator.FieldLevel) bool {
+	regularExp := "^(\\d{17})([0-9]|X|x)$"
 	regExp := regexp.MustCompile(regularExp)
 	mobile, ok := fl.Field().Interface().(string)
 	if !ok {
