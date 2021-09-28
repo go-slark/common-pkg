a12ecca168e350218f5394fadb0255d38c2f26e7
blackroot <longkuicheng@class100.com>
2021-09-28 18:48:36 +0800

    xbus

--- go.mod
+++ go.mod
@@ -9,8 +9,9 @@ require (
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/gomodule/redigo v2.0.0+incompatible
+	github.com/google/uuid v1.3.0
 	github.com/jinzhu/gorm v1.9.16
-	github.com/looplab/eventhorizon v0.14.5
+	github.com/looplab/eventhorizon v0.14.7
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/nickxb/gin-http-logger v1.1.1-0.20191028062356-c63c682255f4
 	github.com/nickxb/pkg v0.6.1
@@ -18,6 +19,7 @@ require (
 	github.com/onsi/gomega v1.16.0 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.4.2
+	github.com/smallfish-root/eventhorizon v1.0.1
 	go.mongodb.org/mongo-driver v1.7.1
 	google.golang.org/grpc v1.39.1
 	gopkg.in/go-playground/validator.v9 v9.28.0
--- xbus/event.go
+++ xbus/event.go
@@ -2,48 +2,116 @@ package xbus
 
 import (
 	"context"
-	"fmt"
+	"github.com/google/uuid"
 	eh "github.com/looplab/eventhorizon"
-	kafkaEvent "github.com/looplab/eventhorizon/eventbus/kafka"
 	"github.com/pkg/errors"
+	"github.com/smallfish-root/eventhorizon"
+	"github.com/smallfish-root/eventhorizon/eventbus/kafka"
+	"time"
 )
 
-var eventBus eh.EventBus
+//event
+//事件数据
 
-// InitEventBus 暂时支持kafka单节点
-func InitEventBus(addr, appId string) {
-	eb, err := kafkaEvent.NewEventBus(addr, appId)
+type EventData = eventhorizon.EventData
+type Event struct {
+	EType eventhorizon.EventType
+	EId   uuid.UUID //失败的需要存起来，定时重新发布 3s
+	EventData
+}
+
+func RegisterEventData(events ...Event) {
+	for _, event := range events {
+		eventhorizon.RegisterEventData(event.EType, func() eventhorizon.EventData { return event })
+	}
+}
+
+func QueryEventData(eventType eventhorizon.EventType) (eventhorizon.EventData, error) {
+	return eventhorizon.CreateEventData(eventType)
+}
+
+//事件需要嵌入到具体的命令中一起执行，另外也需要支持可单端执行(定时执行失败的eid时使用)
+
+var eventBus *kafka.EventBus
+
+func InitEventBus(addr []string, appId string) {
+	bus, err := kafka.NewEventBus(addr, appId)
 	if err != nil {
 		panic(errors.WithStack(err))
 	}
 
-	eventBus = eb
+	eventBus = bus
 }
 
-func WatchEventError() {
-	for err := range eventBus.Errors() {
-		fmt.Printf("event bus error:%+v\n", err)
-	}
+type EventMatcher = eh.EventMatcher
+type EventHandler = eh.EventHandler
+type EventHandlerInfo struct {
+	EventHandler
+	EventMatcher
 }
 
-type EventSet struct {
-	//eh.MatchEvents {}
-	Matcher eh.EventMatcher
-	//event需要实现两个相关的接口
-	Handler eh.EventHandler
-}
+//具体事件需要实现响应的接口函数
+//HandleEvent(ctx context.Context, event eh.Event)
+//HandlerType() eh.EventHandlerType
 
-// RegisterEventHandler 订阅event 处理
-func RegisterEventHandler(eventSet ...*EventSet) {
-	for _, es := range eventSet {
-		err := eventBus.AddHandler(context.TODO(), es.Matcher, es.Handler)
+func RegisterEventHandler(eventHandlerInfos ...EventHandlerInfo) {
+	ctx := context.TODO()
+	for _, eventHandlerInfo := range eventHandlerInfos {
+		//需要重新修改AddHandler函数 --- 删除掉kafka reader部分  add handler是消费event?
+		err := eventBus.AddHandler(ctx, eventHandlerInfo.EventMatcher, eventHandlerInfo.EventHandler)
 		if err != nil {
 			panic(errors.WithStack(err))
 		}
 	}
 }
 
-func HandleEvent(ctx context.Context, event eh.Event) error {
-	//event publish
-	return eventBus.HandleEvent(ctx, event)
+//具体event需要实现Event接口函数
+
+//event注册时重新new一个event
+
+//func NewEvent() *Event {
+//	return eventhorizon.NewEvent("", nil, time.Now())
+//	return &Event{}
+//}
+
+// NewEvent 需要发布的event
+func NewEvent() eh.Event {
+	return eh.NewEvent("", nil, time.Now())
+}
+
+//func (e *Event) EventType() eh.EventType {
+//	return e.EventType()
+//}
+//
+//func (e *Event) Data() eh.EventData {
+//	return nil
+//}
+//
+//func (e *Event) Timestamp() time.Time {
+//	return time.Now()
+//}
+//
+//func (e *Event) AggregateType() eh.AggregateType {
+//	return ""
+//}
+//
+//func (e *Event) AggregateID() uuid.UUID {
+//	return e.EId
+//}
+//
+//func (e *Event) Version() int {
+//	return 0
+//}
+//
+//func (e *Event) Metadata() map[string]interface{} {
+//	return nil
+//}
+//
+//func (e *Event) String() string {
+//	return ""
+//}
+
+func HandleEvent(event *Event) error {
+	//e, _ := QueryEventData("")
+	return eventBus.HandleEvent(context.TODO(), NewEvent())
 }
