0e8acde506a915c29db2188026b7a9424a610125
blackroot <smallfish_root@126.com>
2022-05-24 00:09:02 +0800

    request id

---
+++ xgin/req.go
@@ -0,0 +1,53 @@
+package xgin
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+var requestId string
+
+type config struct {
+	builder   func() string
+	requestId string
+}
+
+type Option func(*config)
+
+func WithBuilder(b func() string) Option {
+	return func(cfg *config) {
+		cfg.builder = b
+	}
+}
+
+func WithRequestId(requestId string) Option {
+	return func(cfg *config) {
+		cfg.requestId = requestId
+	}
+}
+
+func BuildRequestId(opts ...Option) gin.HandlerFunc {
+	cfg := &config{
+		builder: func() string {
+			return uuid.New().String()
+		},
+		requestId: "X-Request-ID",
+	}
+
+	for _, opt := range opts {
+		opt(cfg)
+	}
+
+	return func(ctx *gin.Context) {
+		rid := ctx.GetHeader(cfg.requestId)
+		if rid == "" {
+			rid = cfg.builder()
+		}
+		requestId = cfg.requestId
+		ctx.Header(cfg.requestId, rid)
+	}
+}
+
+func Get(ctx *gin.Context) string {
+	return ctx.Writer.Header().Get(requestId)
+}
