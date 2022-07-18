719fa7b806b182419cb74f2f148cc025208c3b77
blackroot <smallfish_root@126.com>
2022-07-18 21:19:09 +0800

    server timeout interceptor

--- xgrpc/interceptor.go
+++ xgrpc/interceptor.go
@@ -3,6 +3,7 @@ package xgrpc
 import (
 	"context"
 	"google.golang.org/grpc"
+	"sync"
 	"time"
 )
 
@@ -17,5 +18,45 @@ func UnaryClientTimeout() grpc.UnaryClientInterceptor {
 			defer cancel()
 		}
 		return invoker(ctx, method, req, reply, cc, opts...)
+	}
+}
+
+func UnaryServerTimeout(timeout time.Duration) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		ctx, cancel := context.WithTimeout(ctx, timeout)
+		defer cancel()
+
+		var (
+			resp interface{}
+			err  error
+			l    sync.Mutex
+		)
+		done := make(chan struct{})
+		ch := make(chan interface{}, 1)
+		go func() {
+			defer func() {
+				if p := recover(); p != nil {
+					ch <- p
+				}
+			}()
+
+			l.Lock()
+			defer l.Unlock()
+			resp, err = handler(ctx, req)
+			close(done)
+		}()
+
+		select {
+		case p := <-ch:
+			panic(p)
+		case <-done:
+			l.Lock()
+			defer l.Unlock()
+			return resp, err
+		case <-ctx.Done():
+			l.Lock()
+			defer l.Unlock()
+			return nil, ctx.Err()
+		}
 	}
 }
