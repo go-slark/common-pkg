0ef1a3c9f69403d2f9a152e39ff36f94ba6f2d4c
blackroot <smallfish_root@126.com>
2022-08-06 11:15:05 +0800

    proto form

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.8.1
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
+	github.com/go-playground/form/v4 v4.2.0
 	github.com/go-playground/locales v0.14.0
 	github.com/go-playground/universal-translator v0.18.0
 	github.com/go-playground/validator/v10 v10.11.0
---
+++ xencoding/form/form.go
@@ -0,0 +1,56 @@
+package form
+
+import (
+	"github.com/go-playground/form/v4"
+	"github.com/smallfish-root/common-pkg/xencoding"
+	"google.golang.org/protobuf/proto"
+	"net/url"
+	"reflect"
+)
+
+const (
+	Name    = "x-www-form-urlencoded"
+	nullStr = "null"
+)
+
+func init() {
+	decoder := form.NewDecoder()
+	decoder.SetTagName("json")
+	xencoding.RegisterCodec(codec{
+		decoder: decoder,
+	})
+}
+
+type codec struct {
+	decoder *form.Decoder
+}
+
+func (c codec) Marshal(v interface{}) ([]byte, error) {
+	return []byte{}, nil
+}
+
+func (c codec) Unmarshal(data []byte, v interface{}) error {
+	vs, err := url.ParseQuery(string(data))
+	if err != nil {
+		return err
+	}
+
+	rv := reflect.ValueOf(v)
+	for rv.Kind() == reflect.Ptr {
+		if rv.IsNil() {
+			rv.Set(reflect.New(rv.Type().Elem()))
+		}
+		rv = rv.Elem()
+	}
+	if m, ok := v.(proto.Message); ok {
+		return DecodeValues(m, vs)
+	} else if m, ok := reflect.Indirect(reflect.ValueOf(v)).Interface().(proto.Message); ok {
+		return DecodeValues(m, vs)
+	}
+
+	return c.decoder.Decode(v, vs)
+}
+
+func (codec) Name() string {
+	return Name
+}
