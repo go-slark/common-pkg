a5d3c8b4d318a93188d4be106599176b6e72251b
blackroot <longkuicheng@class100.com>
2023-01-11 15:57:03 +0800

    dir

--- xcmd/hack/proto/create.go
+++ xcmd/hack/proto/create.go
@@ -67,7 +67,7 @@ func create(path, dir string) error {
 	fmt.Println("dir:", dir)
 	cmd := []string{
 		"-I=.",
-		"-I=" + dir,
+		"-I=" + "third_party",
 		"--go_out=" + dir,
 		"--go_opt=paths=source_relative",
 		"--go-grpc_out=" + dir,
